bb7ec80296dde095fe236eaad9a2c6a9f8ab3bc7
CDjepeno <djepeno.christopher@gmail.com>
2021-01-06 02:06:08 +0100

    Add picture for fighters

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import fighters from './models/fighters';
+import FIGHTERS from './models/mock-fighters';
   
 const App  : React.FC = () => {
-    const name: String = 'React';
-        
+    const [name, setName] = useState<String>('React');
+    const [Fighters, setFighters] = useState();
+    
     return (
-    <h1>Bonjour, {name} !</h1>
-    )
+   <>
+        <h1>ils y a {FIGHTERS.length} fighthers</h1>
+
+        {FIGHTERS.map(fighter => 
+            <img src={fighter.picture} alt=""/>
+
+        )}
+   </>
+    );
 }
   
 export default App;
