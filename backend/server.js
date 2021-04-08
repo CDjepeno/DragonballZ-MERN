065e586dd3dc0bd6dafa9c0ea241903b4eb5ea80
CDjepeno <djepeno.christopher@gmail.com>
2021-04-08 17:18:01 +0200

    Create database in mongoDB

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,22 @@ dotenv.config()
 const PORT = process.env.PORT || 4000
 const app = express()
 
+/**
+ * middlewares
+ */
+app
+    .use(express.json())
+
+
+/**
+ * Connect MongoDB
+ */
+mongoose.connect(process.env.MONGODB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
+
 app.listen(PORT, () => {
     console.log(`Listening server in PORT ${PORT}`);
 })
